8bfd9740a4d376224d94a4769002af85c4411b94
TarasovVP <vptarasov1998@gmail.com>
2025-01-18 16:43:04 +0200

    Minor fixes

--- app/src/main/java/com/vnteam/submoduleproject/ui/SubModuleClass.kt
+++ app/src/main/java/com/vnteam/submoduleproject/ui/SubModuleClass.kt
@@ -3,12 +3,6 @@ package com.vnteam.submoduleproject.ui
 open class SubModuleClass {
 
     fun textFromSubModule(): String {
-        return "Text from SubModuleClass"
-    }
-
-    fun coreModuleTextFromSubModule(): String {
-        /*val coreModuleClass = CoreModuleClass()
-        return coreModuleClass.textFromCoreModule()*/
-        return ""
+        return "Text from Sub Module"
     }
 }
